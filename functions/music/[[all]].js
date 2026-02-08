94068688427e271cbafbf78768d28e5d530eede7
Brian Beach <coder@beachfamily.net>
2026-02-08 12:49:21 -1000

    URL decode the path before using it as an object name in R2.

--- functions/music/[[all]].js
+++ functions/music/[[all]].js
@@ -2,7 +2,9 @@
 export async function onRequestGet(ctx) {
     const path = new URL(ctx.request.url).pathname.replace("/music/", "");
     console.log("path", path);
-    const file = await ctx.env.music.get(path);
+    const decodedPath = decodeURIComponent(path);
+    console.log("decodedPath", decodedPath);
+    const file = await ctx.env.music.get(decodedPath);
     console.log("file", file);
     if (!file) return new Response("file not found" /*null*/, { status: 404 });
     return new Response(file.body, {
