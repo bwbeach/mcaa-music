7e96fc1a40dd37222805f1a8f4b3131a415aeaf6
Brian Beach <coder@beachfamily.net>
2026-02-08 12:41:14 -1000

    add message string for debugging

--- functions/music/[[all]].js
+++ functions/music/[[all]].js
@@ -1,9 +1,10 @@
 // Based on code here: https://developers.cloudflare.com/pages/tutorials/use-r2-as-static-asset-storage-for-pages/
 export async function onRequestGet(ctx) {
     const path = new URL(ctx.request.url).pathname.replace("/music/", "");
-    console.log("path:", path);
+    console.log("path", path);
     const file = await ctx.env.music.get(path);
-    if (!file) return new Response(null, { status: 404 });
+    console.log("file", file);
+    if (!file) return new Response("file not found" /*null*/, { status: 404 });
     return new Response(file.body, {
 	headers: { "Content-Type": file.httpMetadata.contentType },
     });
