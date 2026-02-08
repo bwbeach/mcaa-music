80a79c22d1fc663ed42023a7d582e172c1436644
Brian Beach <coder@beachfamily.net>
2026-02-08 12:55:42 -1000

    Remove debug logging.

--- functions/music/[[all]].js
+++ functions/music/[[all]].js
@@ -2,11 +2,9 @@
 export async function onRequestGet(ctx) {
     const path = new URL(ctx.request.url).pathname.replace("/music/", "");
     console.log("path", path);
-    const decodedPath = decodeURIComponent(path);
     console.log("decodedPath", decodedPath);
-    const file = await ctx.env.music.get(decodedPath);
     console.log("file", file);
-    if (!file) return new Response("file not found" /*null*/, { status: 404 });
+    if (!file) return new Response(null, { status: 404 });
     return new Response(file.body, {
 	headers: { "Content-Type": file.httpMetadata.contentType },
     });
