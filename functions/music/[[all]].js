d883d83ac4f620dc72321c522dfb845e32bef70c
Brian Beach <coder@beachfamily.net>
2026-02-08 18:44:57 -1000

    Set decodedPath

--- functions/music/[[all]].js
+++ functions/music/[[all]].js
@@ -1,6 +1,7 @@
 // Based on code here: https://developers.cloudflare.com/pages/tutorials/use-r2-as-static-asset-storage-for-pages/
 export async function onRequestGet(ctx) {
     const path = new URL(ctx.request.url).pathname.replace("/music/", "");
+    const decodedPath = decodeURIComponent(path);
     const file = await ctx.env.music.get(decodedPath);
     if (!file) return new Response(null, { status: 404 });
     return new Response(file.body, {
